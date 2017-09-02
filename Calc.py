13bb26f9b6eadbd99bab26d75ce8a8e323e15591
crystalshashi <31563423+crystalshashi@users.noreply.github.com>
2017-09-02 16:53:09 +0530

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,11 @@
+firstnumber = int(input())
+charc = input()
+secondnumber = int(input())
+if charc == '+':
+  print(firstnumber + secondnumber)
+elif charc == '-':
+  print(firstnumber - secondnumber)
+elif charc == '*':
+  print(firstnumber * secondnumber)
+elif charc == '/':
+  print(firstnumber / secondnumber)
